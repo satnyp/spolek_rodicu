d5915918f49efdeb572dc3e805ff02e5677c2e77
Petr Šatný <80728337+satnyp@users.noreply.github.com>
2026-02-21 02:14:47 +0100

    Create firebaseFallback.ts

---
+++ src/config/firebaseFallback.ts
@@ -0,0 +1,9 @@
+export const firebaseFallback = {
+  apiKey: "AIzaSyDddZrdWTcM1qeiXDsAI6LkLbyaL1v-dw0",
+  authDomain: "prispevkyrodicu.firebaseapp.com",
+  projectId: "prispevkyrodicu",
+  storageBucket: "prispevkyrodicu.firebasestorage.app",
+  messagingSenderId: "609240256764",
+  appId: "1:609240256764:web:1639029e9e419a411b0779",
+  measurementId: "G-VVPDGQQJL7",
+} as const;
